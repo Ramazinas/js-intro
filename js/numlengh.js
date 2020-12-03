7969886e3414144e1ea7eb3cca64c32445600ba5
ramazinas <Ramazinas@gmail.com>
2020-12-03 13:39:37 +0200

    Intro 2

---
+++ js/numlengh.js
@@ -0,0 +1,20 @@
+/*
+	console.log( didziausiasSkaiciusSarase( [ 1 ] ) );
+1.	rezultatas: 1
+ii.	console.log( didziausiasSkaiciusSarase( [ 1, 2, 3 ] ) );
+1.	rezultatas: 3
+iii.	console.log( didziausiasSkaiciusSarase( [ -5, 78, 14, 0, 18 ] ) );
+1.	rezultatas: 78
+iv.	console.log( didziausiasSkaiciusSarase( [ 69, 69, 69, 69, 66 ] ) );
+1.	rezultatas: 69
+v.	console.log( didziausiasSkaiciusSarase( [ -1, -2, -3, -4, -5, -6, -7, -8 ] ) );
+1.	rezultatas: -1
+vi.	console.log( didziausiasSkaiciusSarase( “pomidoras” ) );
+1.	rezultatas: “Pateikta netinkamo tipo reikšmė.”
+vii.	console.log( didziausiasSkaiciusSarase( [] ) );
+1.	rezultatas: “Pateiktas sąrašas negali būti tuščias.”
+*/
+
+
+
+function numlenght();
